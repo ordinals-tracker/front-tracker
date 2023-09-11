2e46c294c263af8ab6643bb67ac04a9916145f2e
gynchiii <hiddenk@protonmail.com>
2023-09-11 15:14:28 -0300

    :rocket:

--- quasar.config.js
+++ quasar.config.js
@@ -90,10 +90,6 @@ module.exports = configure(function (/* ctx */) {
       // iconSet: 'material-icons', // Quasar icon set
       // lang: 'en-US', // Quasar language pack
 
-      // For special cases outside of where the auto-import strategy can have an impact
-      // (like functional components as one of the examples),
-      // you can manually specify Quasar components/directives to be available everywhere:
-      //
       // components: [],
       // directives: [],
 
